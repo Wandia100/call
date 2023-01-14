4620cb4e580fdeafa06a08bff8a35b671a2feec2
WANDIA <wandia101>
2023-01-14 12:23:29 +0300

    first commit

---
+++ about.php
@@ -0,0 +1,74 @@
+<?php include('home.php'); ?>
+
+
+
+
+
+	<!-- this is for student registraton -->
+	<div class="main_content" style="background-color:#fff;">
+		<h3 class="text-center" style="background-color:#272327;color: #fff;">About Us</h3>
+		<div class="col-md-12">
+			<div class="col-md-6">
+				<img src="images/wearebesideyou.jfif" alt="" class="img-responsive">
+			</div>
+
+			<div class="col-md-6">
+				<article>
+					<h3 style="color:#0616BC;">We are Beside you</h3>
+					<p class="text-justify">Reasons to Join Pioneer International University
+•    Affordable Fees
+•    Educational loans available from Higher Education Loans Board(HELB)
+•    Strong linkage with Industry for student Internship and placement
+•    Hotspots(Wi-Fi)
+•    Comfortable student hostels separate for male and female students
+•    Strong Career mentorship through strong and resourceful faculty members and vibrant students
+•    Credit transfers/ exemptions given to diploma holders in relevant fields
+•    Spirit of family hood
+•    Flexible scheduling of courses-day, evening and weekends
+•    Serene and conducive learning environment a few minutes from the Nairobi CBD
+
+Courses Offered at Pioneer International University
+Find Courses Offered at Pioneer International University below.</p>
+				</article>
+			</div>
+		</div>
+
+
+		<div class="col-md-12">
+			<div class="col-md-6">
+				<article>
+					<h3 style="color:#0616BC;">24/7 hour service</h3>
+					<p class="text-justify">Pioneer International University is a private institution of higher learning focused on being a world-class market-driven centre of excellence. Accredited in 2012, the institution offers degree, diploma, professional, and certificate courses aimed at advancing knowledge through cutting-edge innovation and research. The university is also uniquely positioned to design and drive a successful entrepreneurial future for students built on innovation, knowledge, and collaboration. Read more: https://www.tuko.co.ke/314798-pioneer-international-university-courses-fee-structure-location.html.Science and Information Communication Technology The School of Science and ICT allows talented students to gain academic experience through the institution’s accredited Bachelor of Science in Information and Technology. After enrolling, students will get courses that respond to the rapid changes in modern technology as well as industry and employer needs. Some of the courses offered include: Read more: https://www.tuko.co.ke/314798-pioneer-international-university-courses-fee-structure-location.
+						</p>
+				</article>
+			</div>
+			<div class="col-md-6">
+				<img src="images/open24rs.jpg" alt="" class="img-responsive"><br>
+			</div>
+		</div>
+          
+    </div>
+		
+	
+	
+	
+
+	
+ <?php include('footer.php'); ?>
+
+
+	
+	</div><!--  containerFluid Ends -->
+
+
+
+
+	<script src="js/bootstrap.min.js"></script>
+
+
+
+
+
+	
+</body>
+</html>
